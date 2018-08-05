e27962ba6c95782e7bf56aa689cc49f0f0c17a0f
Bob Brown <robertbernardbrown@gmail.com>
2018-08-05 14:52:39 -0400

    adds main content to welcome page and buttons to log in or sign up

--- pages/welcome.js
+++ pages/welcome.js
@@ -6,10 +6,23 @@ class WelcomePage extends PolymerElement {
   static get template() {
     return html`
         <style>
-        
+            #button-group {
+                display: flex;
+                justify-content: center;
+            }
         </style>
 
         <header-banner></header-banner>
+            <main>
+                <h2>Welcome to Allowance!</h1>
+                    <p>Allowance is an app to help you keep track of your funds in the simplest way possible</p>
+                    <p>Post a budget and then track your transactions.</p>
+                    <p>That's it - no fancy bank connections, just a good ol' fashioned budget tracker</p>
+                    <div id="button-group">
+                        <button>Sign-Up</button>
+                        <button>Sign-In</button>
+                    </div>
+            </main>
         <footer-banner></footer-banner>
     `;
   }
